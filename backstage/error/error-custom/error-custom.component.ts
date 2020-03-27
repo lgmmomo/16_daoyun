1db87e43ece9a928aa0683027f95233b5d9c1018
yypnogg1011 <675486041@qq.com>
2020-03-27 19:38:09 +0800

    自定义异常

---
+++ backstage/error/error-custom/error-custom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-custom',
+  templateUrl: './error-custom.component.html',
+  styleUrls: ['./error-custom.component.css']
+})
+export class ErrorCustomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
